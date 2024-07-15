67b44e1f3ded870a435fccc88c05f2b11956c0ea
Meerim <kgleader@gmail.com>
2024-07-15 23:55:14 +0530

    dataTypes

---
+++ JS/dataTypes.js
@@ -0,0 +1,20 @@
+//strings
+let box1 = 'Apple'
+console.log(box1)
+//numbers
+let box2 = 123456
+console.log(box2)
+//undefined values
+let box3
+console.log(box3)
+
+//null
+let box4 = null
+
+//boolean : true or false
+let box5 = true
+console.log(box5)
+
+
+
+
