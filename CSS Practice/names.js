101cf72aa68038a2a210509ea83d2d06cafb2b93
Meerim <kgleader@gmail.com>
2024-07-15 23:38:41 +0530

    js codes

---
+++ CSS Practice/names.js
@@ -0,0 +1,21 @@
+let student;
+let name;
+name = 'Alex'
+student = name;
+console.log(student)
+
+let var1, var2, var3, var4;
+
+let var5 = 5, var6 = "Hello", var7 = true, var8 = null;
+
+let firstName = "Meerim";
+let lastName = "Asan";
+let maritalStatus = "Single";
+let country = "Kyrgyzstan";
+let age = 35;
+
+let myAge = 25;
+let yourAge = 28;
+console.log("My age is:", myAge);
+console.log("Your age is:", yourAge);
+
