c29e244432069d2beab19c528133aaf70d408f39
Meerim <kgleader@gmail.com>
2024-07-25 23:11:34 +0530

    switch mode

---
+++ JS/switch.js
@@ -0,0 +1,13 @@
+let day = 'Friday'
+
+switch (day){
+    case 'Monday': console.log('It is Monday')
+    break
+    
+    case 'Tuesday': console.log('It is Tuesday')
+    break
+    
+    case 'Friday': console.log('Yes it is a Friday')
+    break
+    
+}
