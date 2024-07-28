77b399f28c7f1e69973f7e4bff42030483bb39e2
Meerim <kgleader@gmail.com>
2024-07-28 14:07:48 +0530

    number

--- JS/switch.js
+++ JS/switch.js
@@ -30,4 +30,20 @@ if (currentDay >= 1 && currentDay <= 5) {
     console.log(weekendMessage);
 }
 
+function isDivisibleBy5And11(number) {
+    if (number % 5 === 0 && number % 11 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Example usage
+const number = 55; // Change this value to test with different numbers
+if (isDivisibleBy5And11(number)) {
+    console.log(`${number} is divisible by both 5 and 11.`);
+} else {
+    console.log(`${number} is not divisible by both 5 and 11.`);
+}
+
 
