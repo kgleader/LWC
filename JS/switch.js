167116eb4eb8c649a62fc9051752ec8373c76f49
Meerim <kgleader@gmail.com>
2024-07-28 14:15:44 +0530

    getGrdae

--- JS/switch.js
+++ JS/switch.js
@@ -47,3 +47,20 @@ if (isDivisibleBy5And11(number)) {
 }
 
 
+function getGrade(score){
+    if(score >= 80 && score <= 100){
+        return 'A';
+    } else if (score >=70 && score <= 79){
+        return 'B';
+    } else if (score >=60 && score <= 69){
+        return 'C';
+    } else if (score >= 50 && score <= 59){
+        return 'D';
+    } else if (score >=0 && score <= 49){
+        return 'F';
+    } else {
+        return 'Invalid score';
+    }
+}
+
+//
