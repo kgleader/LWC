6051ebc4a3d352ed304f48024bd5bc8c140b4881
Meerim <kgleader@gmail.com>
2024-08-07 00:38:05 +0530

    addition of array

--- JS/arrays.js
+++ JS/arrays.js
@@ -4,3 +4,12 @@ let days = ['Monday', 'Tuesday', 2, 'Wednasday', true, null]
 //2 = 'Wednesday'
 //console.log(days)
 console.log(days[0]) // etc.
+
+let seasons = ['Spring', 'WInter', 'Summer']
+
+//seasons[1]='Autumn' if I want to change the value after
+ 
+console.log(seasons[0])
+
+
+
