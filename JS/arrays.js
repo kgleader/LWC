8a138981ba1d7d53eb5dc830a49691bdf11ca375
Meerim <kgleader@gmail.com>
2024-08-07 00:15:58 +0530

    arrays samples

--- JS/arrays.js
+++ JS/arrays.js
@@ -1,5 +1,6 @@
 let days = ['Monday', 'Tuesday', 2, 'Wednasday', true, null]
-0 = 'Monday'
-1 = 'Tuesday'
-2 = 'Wednesday'
-console.log(days)
+//0 = 'Monday'
+//1 = 'Tuesday'
+//2 = 'Wednesday'
+//console.log(days)
+console.log(days[0]) // etc.
