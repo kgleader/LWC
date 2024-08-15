bb92ed1af0ba93227872e39601aef91e2cc1d4cf
Meerim <kgleader@gmail.com>
2024-08-16 00:39:58 +0530

    practise loop people

--- JS/loops.js
+++ JS/loops.js
@@ -29,7 +29,35 @@ for(let a=0; a<10; a++){
 
 
 
-for (let i = 5; i <= 15; i++) {
-    console.log(i);
-}
+//for (let i = 5; i <= 15; i++) {
+//    console.log(i);
+//}
+
+//Practice 2 (JavaScript)
+//Add numbers from 0 to 10 to the basket list using a for loop:
+//let basket = [];
+//for (let i = 0; i <= 10; i++) {
+//    basket.push(i);
+//}
+//console.log(basket);
+
+//Print only the positive numbers using a for loop:
+//let nums = [-10, -9, -8, -7, -5, 1, 2, 3, 4, 5, 6];
+//for (let i = 0; i < nums.length; i++) {
+//    if (nums[i] > 0) {
+//        console.log(nums[i]);
+//    }
+//}
+
+//convert the strings in the people array to numbers using a for loop:
+
+//const people = ['12', '22', '55', '6', '2'];
+
+//for (let i = 0; i < people.length; i++) {
+//    let number = parseInt(people[i], 10);
+//    console.log(number);
+//}
+
+
+
 
