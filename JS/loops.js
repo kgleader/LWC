fe9bfb04f7af2919a8c8cb4082b773268566358c
Meerim <kgleader@gmail.com>
2024-08-16 00:35:58 +0530

    prcatice loop

--- JS/loops.js
+++ JS/loops.js
@@ -1,3 +1,35 @@
 for(let a=0; a<10; a++){
     console.log('Hello')
 }
+
+//for(let a=0; a<5; a=a+1){
+//    console.log('Hello' + a)
+//}
+
+//for (let i = 0; i < 10; i++) {
+//    console.log('Hello, Salesforce');
+//}
+
+// for (let i = 5; i <= 15; i++) {
+//    console.log(i);
+//}
+
+//for (let i = 20; i <= 50; i++) {
+//    if (i % 2 === 0) {
+//        console.log(i);
+//    }
+//}
+
+//for (let i = 0; i <= 20; i++) {
+//    if (i % 2 !== 0) {
+//        console.log(i);
+//    }
+//}
+
+
+
+
+for (let i = 5; i <= 15; i++) {
+    console.log(i);
+}
+
